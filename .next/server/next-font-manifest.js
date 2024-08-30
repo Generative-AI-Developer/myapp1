e69b7be2d37b3b497bae57f5954d6d6843891e7b
Muhammad Asif <programmerasif@live.com>
2024-08-30 19:42:39 +0500

    Updated after installing and updating Node via nvm

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/muhammad-asif/Desktop/Next_projects/myapp1/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tayljHCQt0yl7+6SCIZZXfhDMmwiRdsAwiM/lbAIMz0=\"\n}"
